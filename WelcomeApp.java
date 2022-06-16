32c46f6d343dd28886eb10f2afef904e031d0742
dash.himanshu.dash@gmail.com <dash.himanshu.dash@gmail.com>
2022-06-16 12:58:36 +0530

    Added my java

---
+++ WelcomeApp.java
@@ -0,0 +1,13 @@
+package java;
+
+import java.util.Iterator;
+
+public class WelcomeApp {
+	public static void main(String[] args) {
+		for (int i = 0; i <10; i++) {
+			System.out.println("Hello Java"+i);
+			
+		}
+		
+	}
+}
