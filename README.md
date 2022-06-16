d625fff31a5096eb0bcb35b9c75458683271e9ae
dash.himanshu.dash@gmail.com <dash.himanshu.dash@gmail.com>
2022-06-16 15:20:46 +0530

    Maven proj added

--- README.md
+++
@@ -1,1 +0,0 @@
-# DevOps updated 123
---
+++ src/test/java/com/example/sunlifedemo/SunlifeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sunlifedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlifeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
